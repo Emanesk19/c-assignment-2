cfc3480125d748b0953542b54271c5e5c065e4c4
Emanesk19 <101937600+Emanesk19@users.noreply.github.com>
2022-06-28 19:05:45 +0300

    assignment

---
+++ assignment 2/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+int []num = new int[6];
+for(int i = 0; i < num.Length - 1; i++)
+{
+    Console.WriteLine("Enter number : "+ (i+1));
+    num[i] = Convert.ToInt32(Console.ReadLine());
+}
+int min = num[0];
+int max = num[0];
+int diff = 0;
+for(int i = 0; i < num.Length - 1; i++)
+{
+    if (num[i] < min)
+    {
+        min = num[i];
+    }
+    else
+    {
+        max = num[i];
+    }
+    diff = max - min;
+}
+Console.WriteLine("Minimum = " + min);
+Console.WriteLine("Maximum = " + max);
+Console.WriteLine("The Maximum  difference between two elements of the array is   = " + diff);
+
+
+
+
